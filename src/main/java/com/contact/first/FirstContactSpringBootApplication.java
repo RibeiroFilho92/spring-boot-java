52f9b855ebea4316ee479bad755ce3ba17cd2906
RibeiroFilho92 <ribeiro.pdf@hotmail.com>
2020-11-05 18:30:41 -0300

    Project created

---
+++ src/main/java/com/contact/first/FirstContactSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.contact.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstContactSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstContactSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contact/first/FirstContactSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contact.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstContactSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
